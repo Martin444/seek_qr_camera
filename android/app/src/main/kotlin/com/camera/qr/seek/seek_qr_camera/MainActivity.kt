9adcf5aeb854e96403935c4a2b41b1620f16dc49
mfarel <mfarel@openpass.com.ar>
2025-03-04 17:49:35 -0300

    add biometric in mainActivity

--- android/app/src/main/kotlin/com/camera/qr/seek/seek_qr_camera/MainActivity.kt
+++ android/app/src/main/kotlin/com/camera/qr/seek/seek_qr_camera/MainActivity.kt
@@ -1,5 +1,27 @@
 package com.camera.qr.seek.seek_qr_camera
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.camera.qr.seek.seek_qr_camera/biometric_auth"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "authenticate") {
+                val api = BiometricAuthApi(this)
+                val request = AuthRequest().apply {
+                    promptMessage = call.argument<String>("promptMessage")
+                }
+                val response = api.authenticate(request)
+                result.success(response.toMap())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
